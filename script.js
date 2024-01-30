b43076b677387a5b2b8b8f62974e8d6a6d97eac7
raja12351 <rajatsingh12351@gmail.com>
2024-01-30 10:19:03 +0530

    final

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const pop_up = document.getElementById("pop-up");
+const pop_up_2 = document.getElementById("pop-up-2");
 const number = document.getElementById("main");
 const lander = document.getElementById("lander-top");
 const datalist = document.getElementById("datalist");
 
 function closePopup(){
     pop_up.style.display = "none";
+    number.style.opacity = 1;
+    lander.style.opacity = 1;
+}
+
+function openPopup(){
+    number.style.opacity = 0.1;
+    lander.style.opacity = 0.1;
+    pop_up_2.style.display = "block";
+}
+
+function closePopup2(){
+    pop_up_2.style.display = "none";
     number.style.opacity = 1;
     lander.style.opacity = 1;
 }
